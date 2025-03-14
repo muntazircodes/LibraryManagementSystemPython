3a03e84ec76f8e5fcf2630b8880f296336c79f04
Muntazir Manan <muntazircodes@gmail.com>
2025-03-15 00:23:18 +0530

    ports and host has been imported from config

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from app import create_app
+import app.config as config
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host=config.HOST, port=config.PORT, debug=True)
