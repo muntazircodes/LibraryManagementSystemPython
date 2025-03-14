37632f9554f1feb48a81bbb06b3233ab026512b0
Muntazir Manan <muntazircodes@gmail.com>
2025-03-14 23:35:26 +0530

    enums has been added

---
+++ app/enums/copy_condition_enum.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class BookConditionEnum(Enum):
+   EXCELENT = 'Excelent'
+   GOOD = 'Good'
+   WORN = 'Worn'
+   TORN = 'Torn'
---
+++ app/enums/user_status_enum.py
@@ -0,0 +1,4 @@
+from enum import Enum
+class UserStatusEnum(Enum):
+   VERIFIED = 'Verified'
+   UNVERIFIED = 'Unverified'
