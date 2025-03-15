8d8baba85162e54c39fc48790175fcfe3c691965
Muntazir Manan <muntazircodes@gmail.com>
2025-03-15 23:03:56 +0530

    imports of model changed as models are splitted in multiple files

--- app/__init__.py
+++ app/__init__.py
@@ -12,9 +12,14 @@ def create_app():
     jwt.init_app(app)
 
     with app.app_context():
-        from app.models.book_model import Books, Copies, Borrowing, Reserve
-        from app.models.libraries_model import Libraries, Racks
-        from app.models.user_model import User, Report
+        from app.models.book_model import Books
+        from app.models.libraries_model import Libraries
+        from app.models.user_model import User
+        from app.models.borrowing_model import Borrowing
+        from app.models.copies_model import Copies
+        from app.models.report_model import Report
+        from app.models.racks_model import Racks
+        from app.models.reserve_model import Reserve
 
         from app.routes.auth.register import register_bp
         from app.routes.auth.login import auth_bp
@@ -25,4 +30,4 @@ def create_app():
     return app
 
 
-__all__ = ["Libraries", "Books", "Copies", "Borrowing", "Reserve", "Location", "User", "Report"]
+__all__ = ["Libraries", "Books", "Copies", "Borrowing", "Reserve", "Racks", "User", "Report"]
