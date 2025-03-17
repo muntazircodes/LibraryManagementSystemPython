aa32b96a6453fe20a9ae5a6e5d41722e66628ca9
Muntazir Manan <muntazircodes@gmail.com>
2025-03-17 12:08:34 +0530

    Library repository refactored

--- app/repositories/library_repository.py
+++ app/repositories/library_repository.py
@@ -1,5 +1,6 @@
 from app.utils.db import db
-from app.models import Libraries, User, Racks
+from app.models import Libraries, User
+from app.enums import LibraryStatusEnum
 
 class LibraryRepository:
 
@@ -13,7 +14,7 @@ class LibraryRepository:
                 lib_license=lib_license,
                 lib_docs=lib_docs,
                 lib_email=lib_email,
-                library_verified=False
+                library_verified=LibraryStatusEnum.UNVERIFIED
             )
             db.session.add(new_library)
             db.session.commit()
@@ -38,46 +39,42 @@ class LibraryRepository:
     def get_library_by_email(lib_email):
         return Libraries.query.filter_by(lib_email=lib_email).first()
 
-    
-    @staticmethod
-    def get_unverified_libraries():
-        return Libraries.query.filter_by(library_verified=False).all()
 
     @staticmethod
-    def library_exists(lib_id):
-        return Libraries.query.filter_by(lib_id=lib_id).first() is not None
-    
-    @staticmethod
     def get_verified_libraries():
-        return Libraries.query.filter_by(library_verified=True).all()
+        return Libraries.query.filter_by(library_verified=LibraryStatusEnum.VERIFIED).all()
     
     @staticmethod
     def check_lib_users(lib_id):
-        lib = LibraryRepository.get_library_by_id(lib_id) 
-        if not lib:
-            return {"message":"No such library"}
+        library = LibraryRepository.get_library_by_id(lib_id) 
+        if not library:
+            return {"message":"Library not found"}
         users = User.query.filter_by(lib_id=lib_id).all()
         if not users:
             return{"message": "The library has no users"}
-        return users
+        else:
+            return users
 
     @staticmethod
     def update_library(lib_id, **kwargs):
         try:
             with db.session.begin():
-                library = Libraries.query.get_or_404(lib_id)
+                library = Libraries.query.get(lib_id)
+                if library:
                 
-                allowed_fields = [
-                    'lib_name', 'lib_address', 'lib_admin', 'lib_license', 
-                    'lib_docs', 'lib_email', 'library_verified'
-                ]
-                
-                for key, value in kwargs.items():
-                    if key in allowed_fields and hasattr(library, key):
-                        setattr(library, key, value)
-                
-                db.session.commit()
-                return library
+                    allowed_fields = [
+                        'lib_name', 'lib_address', 'lib_admin', 'lib_license', 
+                        'lib_docs', 'lib_email', 'library_verified'
+                    ]
+                    
+                    for key, value in kwargs.items():
+                        if key in allowed_fields and hasattr(library, key):
+                            setattr(library, key, value)
+                    
+                    db.session.commit()
+                    return library
+                else:
+                    return {"message": "Library not found"}
         except Exception as e:
             db.session.rollback()
             raise e
@@ -87,9 +84,10 @@ class LibraryRepository:
         try:
             library = Libraries.query.get(lib_id)
             if not library:
-                raise ValueError("Library not found")
-            db.session.delete(library)
-            db.session.commit()
+                return {"message": "Library not found"}
+            else:
+                db.session.delete(library)
+                db.session.commit()
         except Exception as e:
             db.session.rollback()
             raise e
